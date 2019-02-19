ab09f9af3b97860d9ca51f5a4d90e71558f49d11
captain-vk <vk2609@rambler.ru>
2019-02-19 23:31:17 +0300

    #11 Чтение и показ записей из MySQL

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$con = mysqli_connect("localhost", "root", "","deals_allright");
+if ($con == false) {
+   print("Ошибка подключения: ". mysqli_connect_error());
+}
+else {
+   print("Соединение установлено");
+   // выполнение запросов
+  
+}
+
+?>
